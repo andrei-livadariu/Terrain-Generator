acb02432cced5ee208174e6b6b03e28ee6411807
Andrei Livadariu <livadariu.andrei@gmail.com>
2014-02-18 22:33:30 +0100

    - The terrain is now always centered in the scene - The camera rotates and can zoom in and out in play mode - Assets are now using text serialization

---
+++ Assets/Scripts/CenterTerrain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Terrain))]
+[ExecuteInEditMode]
+public class CenterTerrain : MonoBehaviour
+{
+    private Terrain _terrain;
+
+    private void Awake()
+    {
+        _terrain = GetComponent<Terrain>();
+    }
+
+    private void Update()
+    {
+        // Keep the terrain centered
+        transform.position = -_terrain.terrainData.size / 2;
+    }
+}
---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateCamera : MonoBehaviour
+{
+    public const string ZoomAxis = "Mouse ScrollWheel";
+
+    public float RotationSpeed = 10f;
+    public float ZoomSpeed = 2500f;
+    public float ClosestZoom = 340f;
+    public float FarthestZoom = 2500f;
+
+    private void Update()
+    {
+        // Continuously rotate the camera around the scene center
+        transform.RotateAround(Vector3.zero, Vector3.up, RotationSpeed * Time.deltaTime);
+
+        // Handle zooming in and out
+        float distance = Vector3.Distance(transform.position, Vector3.zero);
+        float scroll = Input.GetAxis(ZoomAxis);
+        if ((scroll > 0 && distance > ClosestZoom) ||
+            (scroll < 0 && distance < FarthestZoom))
+        {
+            transform.position += transform.forward.normalized * (ZoomSpeed * Time.deltaTime * Mathf.Sign(scroll));
+        }
+    }
+}
